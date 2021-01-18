4c943f536ff4f540e75d0c024d88d6c0b5a14a1d
Arinas <ars@netempire.de>
2021-01-18 08:16:57 +0100

    Removed jquery from deps

--- simplelightbox.php
+++ simplelightbox.php
@@ -486,10 +486,10 @@ class SimpleLightbox {
 
         $file = get_option('ar_sl_useLegacy') == 0 ? '/dist/simple-lightbox.min.js' : '/dist/simple-lightbox.legacy.min.js';
 
-		wp_enqueue_script('simplelightbox', plugins_url($file, __FILE__), array('jquery'), '2.4.1', true);
+		wp_enqueue_script('simplelightbox', plugins_url($file, __FILE__), array(), '2.7.1', true);
 
 		//== simplelightbox JS hook
-		wp_register_script('simplelightbox-call', plugins_url('/resources/js/setup.simplelightbox.js', __FILE__), array('jquery', 'simplelightbox'), '1.6.0', true);
+		wp_register_script('simplelightbox-call', plugins_url('/resources/js/setup.simplelightbox.js', __FILE__), array(), '2.7.1', true);
 		//== simplelghtbox options
 		wp_localize_script('simplelightbox-call', 'php_vars', $this->options);
 		wp_enqueue_script('simplelightbox-call');
@@ -507,8 +507,8 @@ class SimpleLightbox {
 		wp_enqueue_style('simplelightbox-admin-css', plugins_url('/resources/css/ar-sl-admin.css', __FILE__));
 
 		//== js
-		wp_enqueue_script('simplelightbox-colorpicker', plugins_url('/resources/js/colorpicker.min.js', __FILE__), array( 'jquery' ), '1.6.0', true);
-		wp_enqueue_script('simplelightbox-admin-js', plugins_url('/resources/js/ar-sl-admin.js', __FILE__), array( 'jquery' ), '1.6.0', true);
+		wp_enqueue_script('simplelightbox-colorpicker', plugins_url('/resources/js/colorpicker.min.js', __FILE__), array( 'jquery' ), '2.7.1', true);
+		wp_enqueue_script('simplelightbox-admin-js', plugins_url('/resources/js/ar-sl-admin.js', __FILE__), array( 'jquery' ), '2.7.1', true);
 	}
 
 	//== add settings link on plugin page
